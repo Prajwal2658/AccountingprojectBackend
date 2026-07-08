63e06c3c561edfaa5101ff0a97fac306534dfe9d
Prajwal2658 <prajwalchaudhary2000@gmail.com>
2026-07-08 22:17:40 +0545

    Initial commit

---
+++ AccountingSystem.API/Program.cs
@@ -0,0 +1,18 @@
+﻿using AccountingSystem.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 🔥 PostgreSQL connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("Default")
+    ));
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
---
+++ AccountingSystem.Infrastructure/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingSystem.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        //public DbSet<Account> Accounts => Set<Account>();
+        //public DbSet<Transaction> Transactions => Set<Transaction>();
+    }
+}
