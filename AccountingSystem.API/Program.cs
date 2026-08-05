b593faae1b841fb1ee78d70e4a37e6e170747080
AvishekKhatiwada <Khatiwadaavishek12@gmail.com>
2026-08-05 21:48:00 +0545

    swagger added

--- AccountingSystem.API/Program.cs
+++ AccountingSystem.API/Program.cs
@@ -8,11 +8,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("Default")
     ));
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapControllers();
 
 app.Run();
